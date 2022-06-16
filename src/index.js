5e670993026f8b61eae9567c07eebc082a58832e
mz <emgyrz@gmail.com>
2022-06-16 22:43:57 +0300

    fix webpack config & one swiper make worked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import 'normalize.css';
+import 'normalize.css';
 import './styles/main.scss';
-import './styles/app.js/slider1';
+// import './styles/app.js/slider1';
 
 
 
@@ -12,12 +12,17 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 var swiper = new Swiper(".mySwiper", {
+    modules: [Navigation, Pagination],
     slidesPerView: 1,
     spaceBetween: 10,
     freeMode: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 
@@ -35,6 +40,6 @@ var swiper2 = new Swiper('.slide_2', {
     clickable: true,
   },
 
-  
+
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,16 @@ module.exports = ({develop}) => ({
               type: 'asset/inline'
           },
           {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [ '@babel/preset-env', ]
+              }
+            }
+          },
+          {
               test: /\.html$/i,
               loader: 'html-loader'
           },
